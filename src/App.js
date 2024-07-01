fac045b2cffef42f184f29f964c65152abc2f53f
mfontenlamiro <mfontenlamiro@gmail.com>
2024-07-01 01:04:19 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 1;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
 
-      <div className="buttons"></div>
-      <button>Previuos</button>
-      <button>Next</button>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previuos
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
